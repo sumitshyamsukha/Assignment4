e263c249477a1c1de52b09b6d61cb9045c845c47
Sumit Shyamsukha <sumitzster@gmail.com>
2013-01-08 02:59:45 +0530

    1st

---
+++ Hangman.java
@@ -0,0 +1,93 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.io.IOConsole;
+import acm.io.IODialog;
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
+
+public class Hangman extends ConsoleProgram {
+
+	private HangmanLexicon hl;
+	private static int incorrectGuesses = 0;
+	private String word;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private IOConsole io = getConsole();
+	private IODialog id = getDialog();
+	private boolean guessed = false;
+	private char letters[];
+	private HangmanCanvas canvas;
+
+	public void init() {
+		canvas = new HangmanCanvas();
+		add(canvas);
+		canvas.init();
+	}
+
+	public void run() {
+		char g = '0';
+		hl = new HangmanLexicon();
+		word = hl.getWord(rgen.nextInt(10));
+		letters = new char[word.length()];
+		for (int i = 0; i < letters.length; i++)
+			letters[i] = '-';
+		println("Welcome to Hangman!");
+		while (!checkWord() & incorrectGuesses < 8) {
+			println("The word now looks like this : " + printWord(letters));
+			println("You have " + (8 - incorrectGuesses) + " guesses left.");
+			String guess = io.readLine("Your guess : ");
+			if (guess.length() > 1)
+				println("Invalid input!");
+			else
+				g = Character.toUpperCase(guess.charAt(0));
+			if (!word.contains("" + g)) {
+				incorrectGuesses++;
+				println("There are no " + g + "'s in the word.");
+				canvas.noteIncorrectGuess(g);
+			} else {
+				println("That guess is correct! ");
+				for (int i = 0; i < word.length(); i++) {
+					if (g == word.charAt(i))
+						letters[i] = g;
+				}
+				canvas.displayWord(printWord(letters));
+			}
+		}
+		if (guessed) {
+			println("You guessed the word : " + word);
+			println("You win!");
+		} else {
+			println("You're completely hung!");
+			println("The word was : " + word);
+			println("You lose!");
+		}
+	}
+
+	public String printWord(char[] a) {
+		String word = "";
+		for (int i = 0; i < a.length; i++)
+			word += a[i];
+		return word;
+	}
+
+	public boolean checkWord() {
+		String word = "";
+		for (int i = 0; i < letters.length; i++) {
+			word += letters[i];
+		}
+		if (this.word.equals(word))
+			guessed = true;
+		else
+			guessed = false;
+		return guessed;
+	}
+
+	public static int getIncorrectGuesses() {
+		return incorrectGuesses;
+	}
+
+}
---
+++ HangmanCanvas.java
@@ -0,0 +1,154 @@
+/*
+ * File: HangmanCanvas.java
+ * ------------------------
+ * This file keeps track of the Hangman display.
+ */
+
+import java.awt.Color;
+
+import acm.graphics.GCanvas;
+import acm.graphics.GLabel;
+import acm.graphics.GLine;
+import acm.graphics.GOval;
+
+public class HangmanCanvas extends GCanvas {
+
+	private GLabel word;
+	private GLabel incorrect;
+
+	public void init() {
+		GLine scaffold = new GLine(X_OFFSET, Y_OFFSET, X_OFFSET,
+				SCAFFOLD_HEIGHT);
+		scaffold.setVisible(true);
+		add(scaffold);
+		GLine beam = new GLine(X_OFFSET, Y_OFFSET, BEAM_LENGTH, Y_OFFSET);
+		beam.setVisible(true);
+		add(beam);
+		GLine rope = new GLine(BEAM_LENGTH, Y_OFFSET, BEAM_LENGTH, Y_OFFSET
+				+ ROPE_LENGTH);
+		rope.setVisible(true);
+		add(rope);
+	}
+
+	/** Resets the display so that only the scaffold appears */
+	public void reset() {
+		removeAll();
+		init();
+	}
+
+	/**
+	 * Updates the word on the screen to correspond to the current state of the
+	 * game. The argument string shows what letters have been guessed so far;
+	 * unguessed letters are indicated by hyphens.
+	 */
+	public void displayWord(String word) {
+		//TODO
+	}
+
+	/**
+	 * Updates the display to correspond to an incorrect guess by the user.
+	 * Calling this method causes the next body part to appear on the scaffold
+	 * and adds the letter to the list of incorrect guesses that appears at the
+	 * bottom of the window.
+	 */
+	public void noteIncorrectGuess(char letter) {
+		nextPart();
+		//TODO
+	}
+
+	private void nextPart() {
+		switch (Hangman.getIncorrectGuesses()) {
+		case 1:
+			GOval head = new GOval(BEAM_LENGTH - (HEAD_RADIUS / 2), Y_OFFSET
+					+ ROPE_LENGTH, HEAD_RADIUS, HEAD_RADIUS);
+			head.setVisible(true);
+			add(head);
+			break;
+		case 2:
+			GLine body = new GLine(BEAM_LENGTH, Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS, BEAM_LENGTH, Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH);
+			body.setVisible(true);
+			add(body);
+			break;
+		case 3:
+			GLine leftUpperArm = new GLine(BEAM_LENGTH, Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD, UPPER_ARM_LENGTH,
+					Y_OFFSET + ROPE_LENGTH + HEAD_RADIUS + ARM_OFFSET_FROM_HEAD);
+			leftUpperArm.setVisible(true);
+			add(leftUpperArm);
+			GLine leftLowerArm = new GLine(UPPER_ARM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD,UPPER_ARM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD+LOWER_ARM_LENGTH);
+			leftLowerArm.setVisible(true);
+			add(leftLowerArm);
+			break;
+		case 4:
+			GLine rightUpperArm = new GLine(BEAM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD,BEAM_LENGTH+UPPER_ARM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD );
+			rightUpperArm.setVisible(true);
+			add(rightUpperArm);
+			GLine rightLowerArm = new GLine(BEAM_LENGTH+UPPER_ARM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD,BEAM_LENGTH+UPPER_ARM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + ARM_OFFSET_FROM_HEAD+LOWER_ARM_LENGTH);
+			rightLowerArm.setVisible(true);
+			add(rightLowerArm);
+			break;
+		case 5:
+			GLine leftHip=new GLine(BEAM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH,BEAM_LENGTH-HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH);
+			leftHip.setVisible(true);
+			add(leftHip);
+			GLine leftLeg=new GLine(BEAM_LENGTH-HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH,BEAM_LENGTH-HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH);
+			leftLeg.setVisible(true);
+			add(leftLeg);
+			
+			break;
+		case 6:
+			GLine rightHip = new GLine(BEAM_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH,BEAM_LENGTH+HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH);
+			rightHip.setVisible(true);
+			add(rightHip);
+			GLine rightLeg = new GLine(BEAM_LENGTH+HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH,BEAM_LENGTH+HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH);
+			rightLeg.setVisible(true);
+			add(rightLeg);
+			break;
+		case 7:
+			GLine leftFoot = new GLine(BEAM_LENGTH-HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH,BEAM_LENGTH-HIP_WIDTH-FOOT_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH);
+			leftFoot.setVisible(true);
+			add(leftFoot);
+			break;
+		case 8:
+			GLine rightFoot = new GLine(BEAM_LENGTH+HIP_WIDTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH,BEAM_LENGTH+HIP_WIDTH+FOOT_LENGTH,Y_OFFSET + ROPE_LENGTH
+					+ HEAD_RADIUS + BODY_LENGTH+LEG_LENGTH);
+			rightFoot.setVisible(true);
+			add(rightFoot);
+			break;
+		}
+	}
+
+	/* Constants for the simple version of the picture (in pixels) */
+	private static final int SCAFFOLD_HEIGHT = 360;
+	private static final int BEAM_LENGTH = 144;
+	private static final int ROPE_LENGTH = 18;
+	private static final int HEAD_RADIUS = 36;
+	private static final int BODY_LENGTH = 144;
+	private static final int ARM_OFFSET_FROM_HEAD = 28;
+	private static final int UPPER_ARM_LENGTH = 72;
+	private static final int LOWER_ARM_LENGTH = 44;
+	private static final int HIP_WIDTH = 36;
+	private static final int LEG_LENGTH = 108;
+	private static final int FOOT_LENGTH = 28;
+	private static final int Y_OFFSET = 20;
+	private static final int X_OFFSET = 30;
+}
